12b978a9509fe9a02201584e417699f9d955aa2e
zawho <zthooper2@gmail.com>
2023-03-02 13:34:54 +0800

    Use promise to fix slideshow

--- main.js
+++ main.js
@@ -11,24 +11,33 @@ const slideShow = (() => {
                 slidesArray[i].style.opacity = 1;
         }
     }
-    function changeOpacity() {
-        for (let i = 0; i < slidesArray.length; i++) {
-            slidesArray[i].style.zIndex = i + 1;
-            if (slidesArray[i].style.zIndex === '5') {
-                slidesArray[i].style.opacity -= 0.01;
+    function test() {
+        return new Promise((x, y) => {
+            const z = setInterval(changeOpacity, 30);
+            function changeOpacity() {
+                for (let i = 0; i < slidesArray.length; i++) {
+                    slidesArray[i].style.zIndex = i + 1;
+                    if (slidesArray[i].style.zIndex === '5') {
+                        slidesArray[i].style.opacity -= 0.01;
+                    }
+                    if (slidesArray[i].style.opacity <= 0) {
+                        clearInterval(z);
+                        x();
+                    }
+                }
             }
-        }
+        })
     }
-    function switchSlides() {
+    async function switchSlides() {
         for (let i = 0; i < slidesArray.length; i++) {
             if (slidesArray[i].style.zIndex === '5' && slidesArray[i].style.opacity <= 0) {
                 slidesArray.unshift(...slidesArray.splice(i, 1));
                 slidesArray[i].style.zIndex = i + 1;
-                slidesArray[0].style.opacity = 1;
             }
         }
+        await test();
+        slidesArray[0].style.opacity = 1;
     }
     setSlideOpacity();
-    setInterval(changeOpacity, 30);
-    setInterval(switchSlides, 30);
+    setInterval(switchSlides, 5000);
 })();
