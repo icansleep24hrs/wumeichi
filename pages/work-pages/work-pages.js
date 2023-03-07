61b058b037ddb23cafebc17686ae5bdf7afb0eb3
zawho <zthooper2@gmail.com>
2023-03-07 10:41:25 +0800

    Add basic manual slide module funcs

--- pages/work-pages/work-pages.js
+++ pages/work-pages/work-pages.js
@@ -1,8 +1,21 @@
 // Manual slideshow for work pages.
 const manualSlideShow = (() => {
+    const picnicSlides = document.querySelectorAll('.picnic-slideshow > img');
+    const picnicSlidesArr = Array.from(picnicSlides);
 
+    function setZIndex() {
+        for (let i = 0; i < picnicSlidesArr.length; i++) {
+            picnicSlidesArr[i].style.zIndex = i + 1;
+        }
+    }
+
+    function changeSlides() {
+        for (let i = 0; i < picnicSlidesArr.length; i++) {
+            if (picnicSlidesArr[i].style.zIndex === '5' && picnicSlidesArr[i].style.opacity <= 0) {
+                picnicSlidesArr.unshift(...picnicSlidesArr.splice(i, 1));
+                picnicSlidesArr[i].style.zIndex = i + 1;
+            }
+        }
+    }
+    setZIndex();
 })();
-
-const picnicSlides = document.querySelectorAll('.picnic-slideshow > img');
-const picnicSlidesArr = Array.from(picnicSlides);
-console.log(picnicSlidesArr);
