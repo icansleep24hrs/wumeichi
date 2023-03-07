8204da8b6c0f82450ed1b26bc33a328e0e5ecade
zawho <zthooper2@gmail.com>
2023-03-07 11:17:17 +0800

    Fix manual slides display

--- pages/work-pages/work-pages.js
+++ pages/work-pages/work-pages.js
@@ -7,17 +7,23 @@ const manualSlideShow = (() => {
     function setZIndex() {
         for (let i = 0; i < picnicSlidesArr.length; i++) {
             picnicSlidesArr[i].style.zIndex = i + 1;
+            if (!(picnicSlidesArr[i].style.zIndex === '11')) {
+                picnicSlidesArr[i].style.display = 'none';
+            }
         }
     }
 
     function changeSlides() {
         for (let i = 0; i < picnicSlidesArr.length; i++) {
             if (picnicSlidesArr[i].style.zIndex === '11') {
+                picnicSlidesArr[i].style.display = 'block';
                 picnicSlidesArr.unshift(...picnicSlidesArr.splice(i, 1));
                 picnicSlidesArr[i].style.zIndex = i + 1;
+                picnicSlidesArr[i].style.display = 'none';
             }
         }
     }
     setZIndex();
+    changeSlides();
     slidesButton.addEventListener('click', changeSlides);
 })();
