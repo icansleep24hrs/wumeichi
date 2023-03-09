68817e27846adbe3229c4fb174d01e428255c5e0
zawho <zthooper2@gmail.com>
2023-03-09 12:08:04 +0800

    Update xxy slideshow class

--- pages/work-pages/work-pages.js
+++ pages/work-pages/work-pages.js
@@ -2,7 +2,7 @@
 const manualSlideShow = (() => {
     const forwardButton = document.querySelector('.forward-button');
     const backButton = document.querySelector('.back-button');
-    const allSlides = document.querySelectorAll('.picnic-slideshow img, .pandora-slideshow img');
+    const allSlides = document.querySelectorAll('.picnic-slideshow img, .pandora-slideshow img, .xxy-slideshow img');
     let currentSlide = 0;
     
     function changeSlide(x) {
